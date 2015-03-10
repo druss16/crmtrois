937e673f4aa155c98450d7ceb4546d464ed716a7
druss16 <druss16@gmail.com>
2015-03-09 23:11:42 -0400

    created the hhome page URL conf

--- djangoproject/crmeasy/urls.py
+++ djangoproject/crmeasy/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from marketing.views import HomePage
+#from django.contrib import admin
 
 urlpatterns = patterns('',
+	
+	# Marketing pages
+	url(r'^$', HomePage.as_view(), name="home"),
     # Examples:
     # url(r'^$', 'crmeasy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
